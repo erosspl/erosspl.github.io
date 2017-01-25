bf0c27b6a54e564ef3206d850eadad42648b68a4
Pawel Glowacki <eros.z@poczta.fm>
2017-01-25 20:03:33 +0100

    v0.3

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 $(function () {
+	$('body').hide().fadeIn(3000);
+
 	$('#amiWeb').click(function() {
 		window.open('https://erosspl.github.io/amiga', '_blank');
 	});
@@ -23,4 +25,16 @@ $(function () {
 	$('#rwdGit').click(function() {
 		window.open('https://github.com/erosspl/psd-rwd', '_blank');
 	});
+	$('#goUp').click(function() {
+		window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+	});
+	$('#goDownload').click(function() {
+		document.querySelector('#download').scrollIntoView({ behavior: 'smooth' });
+	});
+	$('#goTechs').click(function() {
+		document.querySelector('#techs').scrollIntoView({ behavior: 'smooth' });
+	});
+	$('#goGallery').click(function() {
+		document.querySelector('#gallery').scrollIntoView({ behavior: 'smooth' });
+	});	
 })
